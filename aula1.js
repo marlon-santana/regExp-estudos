475f2fdb47eb905f02c4bb385248995bf5d32906
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2021-12-08 15:38:42 -0300

    aula1

---
+++ aula1.js
@@ -0,0 +1,7 @@
+
+
+const { text } = require('./texto');
+
+const regExp1 = /web/gi;
+
+console.log(regExp1.exec(text)[0])
---
+++ texto.js
@@ -0,0 +1,16 @@
+const text = `Em JavaScript o String replace é utilizado com frequência para fazer
+ desde simples substituições em Strings até complexas mudanças com expressões regulares.
+  Seu uso é simples como variavel.replace('valor', 'substituicao'). 
+  Eu tenho certeza que alguma vez você já precisou pegar um trecho de texto no final
+   de um HTML, ou remover uma palavra específica de um código.
+
+Nos formulários da web é comum termos de lidar com o campo de CPF. 
+O que realmente importa quando colocamos um campo desses são os números, 
+mas construindo interfaces para o usuário final é super importante mostrar para
+ o usuário a formatação como ele está acostumado a ver nos documentos. 
+ Vamos ver como poderiamos resolver esse problema com os poderes do JavaScript
+  e sua biblioteca padrão.WEB`;
+
+  module.exports = {
+      text
+  }
